74c96a880222bcbeecd90abf9d9304deeda16b14
Qualpo <toxixbeluga@gmail.com>
2023-09-17 00:33:38 -0700

    Update Gun.gd

--- scripts/resources/Gun.gd
+++ scripts/resources/Gun.gd
@@ -7,9 +7,10 @@ class_name Gun
 @export var shoot_cooldown = 0.1
 @export var recoil = 3.0
 @export var bullet_ammount = 1
+@export_range(0,1) var aim_zoom = 0.0
 @export var shoot_sound : AudioStream
 @export var no_bullet_sound : AudioStream
-@export_range(0,1) var aim_zoom = 0.0
+
 
 func SaveItem():
 	var dict = {}
