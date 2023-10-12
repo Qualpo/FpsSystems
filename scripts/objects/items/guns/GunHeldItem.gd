1e17c927645dc96446b40c50d643e7a1074c74d0
Qualpo <toxixbeluga@gmail.com>
2023-10-12 12:44:39 -0700

    Update GunHeldItem.gd

--- scripts/objects/items/guns/GunHeldItem.gd
+++ scripts/objects/items/guns/GunHeldItem.gd
@@ -3,11 +3,13 @@ class_name HeldGun
 
 var Shooting = false
 var Aiming = false
+var CanShoot = true
 
 @onready var RNG = RandomNumberGenerator.new()
 @onready var BulletHole = preload("res://scenes/objects/BulletHole.tscn")
 
 func _ready():
+	super._ready()
 	User.sprint.connect(UserSprint)
 	User.unsprint.connect(UserUnSprint)
 	User.crouch.connect(UserCrouch)
@@ -22,6 +24,7 @@ func Use():
 func UnHold():
 	User.SetFov(0.0)
 	User.SetMovementSpeed(1)
+	User.SetSensitivityScale(1)
 	super.UnHold()
 	
 func  SecondaryUse():
@@ -29,7 +32,7 @@ func  SecondaryUse():
 func SecondaryUnUse():
 	UnAim()
 func Shoot():
-	if not Shooting and item.ammo > 0:
+	if not Shooting and item.ammo > 0 and CanShoot:
 		item.ammo -= 1
 		Shooting = true
 		$Shoot.play()
@@ -50,24 +53,35 @@ func Shoot():
 			Cast.position = Vector3()
 			Cast.enabled = true
 			Cast.target_position = Vector3(0,0,-1000)
-			Cast.collision_mask = 5
+			Cast.collision_mask = 35
 			Cast.rotation_degrees = Vector3(spread.x,spread.y,0)
 			Cast.force_raycast_update()
 		for Cast in $Bullets.get_children():
 			if Cast.is_colliding():
 				
-			
-				if Cast.get_collider().is_in_group("Enemy"):
-					Cast.get_collider().Hit(Global.GunDamage)
-				
+				var Hit = Cast.get_collider()
+				if Hit.is_in_group("Enemy"):
+					Hit.Hit(item.damage)
+				if Hit.is_in_group("Props"):
+					if Hit is RigidBody3D:
+						print("proip")
+						var dir = Vector3()
+						dir = Vector3.FORWARD.rotated(Vector3(1,0,0),global_rotation.y)
+						dir = dir.rotated(Vector3(0,1,0),global_rotation.x)
+						print(global_rotation)
+						Hit.apply_impulse(dir * item.damage/10,Cast.get_collision_point())
 				var hole = BulletHole.instantiate()
 					
-				Cast.get_collider().add_child(hole)
+				Hit.add_child(hole)
 				hole.global_position = Cast.get_collision_point() + (Cast.get_collision_normal()/80)
-				if abs(Cast.get_collision_normal().y) == 1:
-					hole.rotation_degrees.x = 90
-				else:
-					hole.look_at(Cast.get_collision_point()-Cast.get_collision_normal(),Vector3.UP)
+				hole.global_rotation = Vector3()
+				var normal : Vector3 = Cast.get_collision_normal()
+				var bulrot = Vector3()
+				bulrot.x += Vector2(abs(normal.x)+abs(normal.z)/2,-normal.y).angle()
+				bulrot.y += Vector2(normal.x,-normal.z).angle() + deg_to_rad(90)
+				bulrot.z += deg_to_rad(RNG.randf_range(0.0,360.0))
+				hole.global_rotation = bulrot
+				#hole.look_at(Cast.get_collision_point()-Cast.get_collision_normal(),Vector3.UP)
 			Cast.queue_free()
 			
 		if Aiming:
@@ -77,23 +91,24 @@ func Shoot():
 
 		await get_tree().create_timer(item.shoot_cooldown).timeout
 		Shooting = false
-	elif not Shooting:
+	elif not Shooting and CanShoot:
 		$NoBullet.play()
 func Recoil(force):
 	var yf =  RNG.randf_range(-force,force)
 	var xf =  RNG.randf_range(-force,force)
 	User.CameraOffset = Vector2(xf,yf)*0.5
-	User.MoveCamera(Vector2(xf,yf))
+	User.MoveCamera(Vector2(xf/(User.Sensitivity * User.SensitivityScale),yf/(User.Sensitivity * User.SensitivityScale)))
 func Aim():
 	if not Aiming:
 		User.UnSprint()
 		Aiming = true
 		$AnimationPlayer.play("Aim")
-		Shooting = true
+		CanShoot = false
 		await $AnimationPlayer.animation_finished
-		Shooting = false
+		CanShoot = true
 		$AnimationPlayer.play("AimIdle")
 		User.SetFov(-item.aim_zoom * 74)
+		User.SetSensitivityScale(1.0 - item.aim_zoom + 0.01)
 		if User.Crouching:
 			User.SetMovementSpeed(0.5* User.CrouchSpeed)
 		elif User.Sliding:
@@ -107,9 +122,12 @@ func UnAim():
 		await $AnimationPlayer.animation_finished
 		$AnimationPlayer.play("Idle")
 		User.SetFov(0.0)
+		User.SetSensitivityScale(1.0)
 		if User.Crouching:
 			User.SetMovementSpeed(User.CrouchSpeed)
 		elif User.Sliding:
+			pass
+		elif User.Sprinting:
 			pass
 		else:
 			User.SetMovementSpeed(1)
